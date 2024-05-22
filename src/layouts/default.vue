e53e55ca5091bb4cd05f619f36b1147f53f891ee
Antoine LEVEQUE <antoine.leveque@datadoghq.com>
2024-05-22 15:58:22 +0200

    style: add style for login, home and scan pages

---
+++ src/layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+useHead({
+  title: "snackEDU",
+  meta: [{ name: "theme-color", content: "#FFFFFF" }],
+});
+</script>
+
+<template>
+  <VitePwaManifest />
+
+  <div class="h-screen bg-white flex flex-col py-6 font-outfit">
+    <slot />
+  </div>
+</template>
---
+++ src/pages/login.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1
+    class="text-3xl text-black font-bold flex items-center justify-center pt-1"
+  >
+    snackEDU
+  </h1>
+
+  <div class="flex flex-1 justify-center items-center overflow-hidden">
+    <img
+      src="~/assets/images/bg.png"
+      alt="background"
+      class="object-fill w-full h-5/6"
+    />
+  </div>
+
+  <div class="px-8 w-full flex flex-col">
+    <section class="h-36 flex items-center justify-center flex-col">
+      <h3
+        class="text-4xl text-black font-semibold flex items-center justify-center w-full"
+      >
+        Get the
+      </h3>
+      <h3
+        class="text-4xl text-gray-400 font-semibold flex items-center justify-center w-full"
+      >
+        best snack !
+      </h3>
+    </section>
+    <button
+      type="button"
+      class="inline-block rounded-3xl h-[52px] bg-black px-6 pb-2 pt-2.5 text-xs font-medium leading-normal text-gray-200"
+    >
+      Log In
+    </button>
+  </div>
+</template>
