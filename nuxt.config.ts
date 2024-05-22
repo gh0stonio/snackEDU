176ea7e7a5ed2fbcbd5e8e7d5faa7f45cfc9556e
Antoine LEVEQUE <antoine.leveque@datadoghq.com>
2024-05-22 12:58:25 +0200

    chore: add PWA support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,44 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+
+  modules: ["@vite-pwa/nuxt"],
+
+  pwa: {
+    manifest: {
+      name: "SnackEDU",
+      short_name: "SnackEDU",
+      description: "Datadog snack helper",
+      lang: "en",
+      scope: "/",
+      display: "standalone",
+      background_color: "#FFFFFF",
+      theme_color: "#FFFFFF",
+      orientation: "portrait",
+      icons: [
+        {
+          src: "/icons/icon_64.png",
+          sizes: "64x64",
+          type: "image/png",
+        },
+        {
+          src: "/icons/icon_144.png",
+          type: "image/png",
+          sizes: "144x144",
+        },
+        {
+          src: "/icons/icon_192.png",
+          type: "image/png",
+          sizes: "192x192",
+        },
+        {
+          src: "/icons/icon_512.png",
+          type: "image/png",
+          sizes: "512x512",
+        },
+      ],
+    },
+    workbox: { navigateFallback: "/index.html" },
+    devOptions: { enabled: true, type: "module" },
+  },
 });
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,30 @@
+<script setup lang="ts">
+useHead({
+  title: "SnackEDU",
+  meta: [{ name: "theme-color", content: "#FFFFFF" }],
+});
+</script>
+
 <template>
-  <div class="h-screen bg-white flex flex-col py-8 font-outfit">
+  <VitePwaManifest />
+
+  <div class="h-screen bg-white flex flex-col py-6 font-outfit justify-between">
     <h1
-      class="text-3xl text-black font-bold flex items-center justify-center uppercase pt-4"
+      class="text-3xl text-black font-bold flex items-center justify-center uppercase pt-1"
     >
       SnackEDU
     </h1>
 
-    <img src="~/assets/images/bg.png" alt="background" class="grow" />
+    <div class="flex-1 justify-center items-center overflow-hidden">
+      <img
+        src="~/assets/images/bg.png"
+        alt="background"
+        class="object-fill w-full h-full"
+      />
+    </div>
 
     <div class="px-8 w-full flex flex-col">
-      <section class="h-40 flex items-center justify-center flex-col">
+      <section class="h-36 flex items-center justify-center flex-col">
         <h3
           class="text-4xl text-black font-semibold flex items-center justify-center w-full"
         >
