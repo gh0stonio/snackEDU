65e7842920d6e1d28b5aa772db0490e3abb7d15e
Antoine LEVEQUE <antoine.leveque@datadoghq.com>
2024-05-23 15:57:09 +0200

    feat: add ranking nav item

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -30,6 +30,20 @@
         />
       </svg>
     </NuxtLink>
+    <NuxtLink to="/ranking" class="w-10 h-10 flex items-center justify-center" :replace="true">
+      <svg
+        id="Icons"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-6 h-6"
+        :class="route.path === '/ranking' ? 'fill-gray-200' : 'fill-zinc-600'"
+      >
+        <title />
+        <path
+          d="M22,7H16.333V4a1,1,0,0,0-1-1H8.667a1,1,0,0,0-1,1v7H2a1,1,0,0,0-1,1v8a1,1,0,0,0,1,1H22a1,1,0,0,0,1-1V8A1,1,0,0,0,22,7ZM7.667,19H3V13H7.667ZM14.333,8V19H9.667V5h4.666ZM21,19H16.333V9H21Z"
+        />
+      </svg>
+    </NuxtLink>
     <NuxtLink to="/scan" class="w-10 h-10 flex items-center justify-center" :replace="true">
       <svg
         viewBox="0 0 24 24"
