18b7bfa4db6d057b792ad150231be6d563f278c7
ranyakhemiri <rania.khemiri@datadoghq.com>
2024-05-23 15:59:20 +0200

    v0 of ranking page

--- src/pages/ranking.vue
+++ src/pages/ranking.vue
@@ -4,16 +4,31 @@ definePageMeta({
   layout: false,
 });
 
-
-
+const rankedProducts = ref<string[]>([]); // To store the ranked product barcodes
 
 onMounted(async () => {
-
+  try {
+    rankedProducts.value = await rankProducts();
+  } catch (error) {
+    console.error("Failed to fetch ranked products:", error);
+  }
 });
-
-
 </script>
 
 <template>
+  <div class="h-[calc(100dvh)] bg-black flex flex-col font-outfit py-6">
+    <header class="w-full h-14 flex justify-between items-center">
+      <h1 class="basis-1/3 text-lg text-white flex justify-center items-center">Product Rankings</h1>
+      <div class="basis-1/3"></div>
+    </header>
 
+    <main class="w-full px-6 h-full">
+      <h2 class="text-xl text-white mt-6">Ranked Products</h2>
+      <ul class="text-white">
+        <li v-for="(barcode, index) in rankedProducts" :key="index">
+          {{ index + 1 }}. {{ barcode }}
+        </li>
+      </ul>
+    </main>
+  </div>
 </template>
